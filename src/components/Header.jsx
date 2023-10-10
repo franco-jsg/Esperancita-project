992c6840d39e0a67a53d912f3d3edf524b244dc5
Francisco Suarez <franco.jsg@gmail.com>
2023-10-09 21:47:06 -0300

    Logo header to home

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,27 @@
 import { Link } from "react-router-dom";
 
-import Logo from "../assets/svg/header/Group 2.svg"
+import Logo from "../assets/svg/header/Group 2.svg";
 
 const Header = () => {
   return (
     <header className="header">
       <nav className="navbar container">
         <div className="navbar__brand">
-          <img src={Logo} alt="" className="navbar__logo" />
+          <Link to="/">
+            <img src={Logo} alt="" className="navbar__logo" />
+          </Link>
           <p>Fix it in post</p>
         </div>
         <ul className="navbar__list">
-          <Link to="/work" className="navbar__list__item">Work</Link>
-          <Link to="/about" className="navbar__list__item">About</Link>
-          <Link to="/en" className="navbar__list__item">ES/EN</Link>
+          <Link to="/work" className="navbar__list__item">
+            Work
+          </Link>
+          <Link to="/about" className="navbar__list__item">
+            About
+          </Link>
+          <Link to="/en" className="navbar__list__item">
+            ES/EN
+          </Link>
         </ul>
       </nav>
     </header>
