45690afacaffb35ab60288653aefc3bed043b904
Victor Aguilera <vaguilera@192.168.1.5>
2023-11-05 11:37:47 -0300

    Update social icons

--- src/components/Main_social_icons.jsx
+++ src/components/Main_social_icons.jsx
@@ -21,13 +21,13 @@ const Main_social_icons = () => {
   return (
     <div className="main_social_icons" ref={ref}>
       <div className="social__icons">
-        <a href="https://instagram.com" target="_blank" className="social-link">
+        <a href="https://www.instagram.com/esperancitavfx/" target="_blank" className="social-link">
           <img src={Instagram} alt="Instagram link" />
         </a>
         <a href="https://www.youtube.com" target="_blank" className="social-link">
           <img src={YouTube1} alt="YouTube link" />
         </a>
-        <a href="https://www.linkedin.com" target="_blank" className="social-link">
+        <a href="https://www.linkedin.com/company/esperancitavfx/about/" target="_blank" className="social-link">
           <img src={LinkedIn} alt="LinkedIn link" />
         </a>
         <a href="https://www.whatsapp.com" target="_blank" className="social-link">
