406444c753258be3af9c5bc3799e4ff74e21e69d
Francisco Suarez <franco.jsg@gmail.com>
2023-11-10 22:56:12 -0300

    form config with mailto

--- src/components/Footer/Contact_form.jsx
+++ src/components/Footer/Contact_form.jsx
@@ -1,49 +1,47 @@
+import { useNavigate } from "react-router-dom";
+import Arrow from "../../assets/svg/footer/arrow.svg";
+import { useEsperancitaContext } from "../../context/Context";
 
-import Arrow from "../../assets/svg/footer/arrow.svg"
-
-const sendEmail = (e) => {
-  e.preventDefault();
-
-  console.log("Mail enviado!");
-
-  //   emailjs.sendForm(
-  //     "service_nyafgyj",
-  //     "template_shyy32s",
-  //     form.current,
-  //     "1io3oV6uKN71Ho0lS"
-  //   );
-  e.target.reset();
-};
 
 const Contact_form = () => {
+  const navigate = useNavigate()
+  const { lenguage} = useEsperancitaContext()
+
+  const sendEmail = () => {
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+    window.location.href = `mailto:email@example.com?subject=${name +" --- " + email}&body=Message: ${message}`
+  
+    lenguage === "ES" ? navigate('/') : navigate('/en')
+  
+  };
   return (
     <>
       <div className="contact__content">
         <h4 className="contact__title">Contact us</h4>
-        <form onSubmit={sendEmail} className="contact__form">
+        <form
+          className="contact__form"
+          encType="text/plain"
+        >
           <div className="contact__form-div">
-            <input
-              type="text"
-              name="name"
-              placeholder="name"
-            />
+            <input id="name" type="text" name="name" placeholder="name" />
           </div>
 
           <div className="contact__form-div">
-            <input
-              type="email"
-              name="email"
-              placeholder="email"
-            />
+            <input id="email" type="email" name="email" placeholder="email" />
           </div>
 
           <div className="contact__form-div form-area">
             <textarea
+              id="message"
               name="message"
               placeholder="message"
               rows="1"
             ></textarea>
-            <button className=""><img src={Arrow} alt="" /></button>
+            <button className="" onClick={sendEmail}>
+              <img src={Arrow} alt="" />
+            </button>
           </div>
         </form>
       </div>
