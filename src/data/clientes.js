88a70732bcc34e91249b8f8e45db40ae8843dc1a
Francisco Suarez <franco.jsg@gmail.com>
2023-11-23 20:53:11 -0300

    new logos in white

--- src/data/clientes.js
+++ src/data/clientes.js
@@ -3,98 +3,98 @@ import honda from "../assets/svg/clients/honda.svg";
 import nissan from "../assets/svg/clients/nissan-logo.svg";
 import oxxo from "../assets/svg/clients/oxxo.svg";
 import ram from "../assets/svg/clients/ram.svg";
-import bbva from "../assets/svg/clients/BBVA_2019.svg";
-import diddi from "../assets/svg/clients/didi-chuxing.svg";
-import fargo from "../assets/svg/clients/fargo2.svg";
-import bancoAzteca from "../assets/svg/clients/Logo_Banco_Azteca2.svg";
-import bancoCoppel from "../assets/svg/clients/Logo_de_BanCoppel.svg";
-import totalPlay from "../assets/svg/clients/Logo_TotalPlay.svg";
+import bbva from "../assets/svg/clients/2.0/BBVA BLANCO.svg";
+import diddi from "../assets/svg/clients/2.0/didi-BLANCO.svg";
+import fargo from "../assets/svg/clients/2.0/fargo_BLANCO.svg";
+import bancoAzteca from "../assets/svg/clients/2.0/Logo_Banco_Aztecablanco.svg";
+import bancoCoppel from "../assets/svg/clients/2.0/COPPEL BLANCO.svg";
+import totalPlay from "../assets/svg/clients/2.0/Logo_TotalPlay_BLANCO.svg";
 import mg from "../assets/svg/clients/mg3.svg";
-import secret from "../assets/svg/clients/Secret_logo2.svg";
+import secret from "../assets/svg/clients/2.0/Secret_logo_blanco.svg";
 import whirlpool from "../assets/svg/clients/Whirlpool_Corporation.svg";
 import saladitas from "../assets/svg/clients/logo-saladitas.svg";
-import audi from "../assets/svg/clients/audi.svg"
+import audi from "../assets/svg/clients/2.0/Audi-Logo_BLANCO.svg"
 
 const clientes = [
   {
-    name: "Aeromexico",
+    name: "Audi",
     id: 0,
+    logo: audi,
+  },
+  {
+    name: "Aeromexico",
+    id: 1,
     logo: aeroMex,
   },
   {
     name: "Honda",
-    id: 1,
+    id: 2,
     logo: honda,
   },
   {
     name: "Nissan",
-    id: 2,
+    id: 3,
     logo: nissan,
   },
   {
     name: "Oxxo",
-    id: 3,
+    id: 4,
     logo: oxxo,
   },
   {
     name: "Ram",
-    id: 4,
+    id: 5,
     logo: ram,
   },
   {
     name: "BBVA",
-    id: 5,
+    id: 6,
     logo: bbva,
   },
   {
     name: "Diddi",
-    id: 6,
+    id: 7,
     logo: diddi,
   },
   {
     name: "Fargo",
-    id: 7,
+    id: 8,
     logo: fargo,
   },
   {
     name: "Banco Azteca",
-    id: 8,
+    id: 9,
     logo: bancoAzteca,
   },
   {
     name: "Banco Coppel",
-    id: 9,
+    id: 10,
     logo: bancoCoppel,
   },
   {
     name: "Total Play",
-    id: 10,
+    id: 11,
     logo: totalPlay,
   },
   {
     name: "MG",
-    id: 11,
+    id: 12,
     logo: mg,
   },
   {
     name: "Secret",
-    id: 12,
+    id: 13,
     logo: secret,
   },
   {
     name: "Whirlpool",
-    id: 13,
+    id: 14,
     logo: whirlpool,
   },
   {
     name: "Saladitas",
-    id: 14,
-    logo: saladitas,
-  },
-  {
-    name: "Audi",
     id: 15,
-    logo: audi,
+    logo: saladitas,
   }
 ];
 
