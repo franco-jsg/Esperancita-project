51874f6a586b9e23faf63ebaceb3d53b1f65149a
Matias <matidominguez014@gmail.com>
2024-01-29 19:48:30 -0300

    New comercial

--- src/data/comerciales.js
+++ src/data/comerciales.js
@@ -19,7 +19,7 @@ import RAM from "../assets/videos/3d/RAM_DC_MASTER.mp4"
 import TOTALPLAY from "../assets/videos/3d/TOTAL_PLAY_UNIVERSO_MASTER_06_07_23.mp4"
 import About from "../assets/videos/about/Logo_Esperancitavfx_cierre_v03.mp4"
 import Lancomer from "../assets/videos/3d/¡Ya llegó nuestra Temporada Naranja! 🧡 _ La Comer_1.mp4"
-
+import Audi from "../assets/videos/3d/Audi R8. Un legado inolvidable..mp4"
 
 const data = [
   {
@@ -212,7 +212,7 @@ const data = [
     name: "Belight Coco",
     id: 15,
     category: ["Simulation"],
-    description: "Belight coco",
+ 
     description: ["Belight coco", "Belight coco"],
     year: "2022",
     date: "24/04/2022",
@@ -298,6 +298,18 @@ const data = [
     date: "",
     // video: "src/assets/videos/reels/REEL_PANTALLAS_v03.mp4",
     video: About,
+  },
+  {
+    name: "Audi",
+    id: 123,
+    category: ["3D"],
+    // category: ["3D", "Compo"],
+    // description: "3D / motion graphics",
+    description: ["3D / motion graphics", "3D/motion graphics"],
+    year: "2024",
+    date: "18/03/2023",
+    // video: "src/assets/videos/reels/ESPERANCITA_REEL_2023_v12.mp4",
+    video: Audi,
   },
     // {
   //   name: "CHIREY",
