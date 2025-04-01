529c941f35efc94798e0cc47c17c905d462e05a7
t0scoh <158074966+t0scoh@users.noreply.github.com>
2025-04-01 16:41:37 -0300

    a

--- src/data/comerciales.js
+++ src/data/comerciales.js
@@ -18,7 +18,7 @@ import Whirpool from "../assets/videos/3d/Whirpool_60s_12012022_Inglés Master.m
 import RAM from "../assets/videos/3d/RAM_DC_MASTER.mp4";
 import TOTALPLAY from "../assets/videos/3d/TOTAL_PLAY_UNIVERSO_MASTER_06_07_23.mp4";
 import About from "../assets/videos/about/Logo_Esperancitavfx_cierre_v03.mp4";
-import Lancomer from "../assets/videos/3d/¡Ya llegó nuestra Temporada Naranja! 🧡 _ La Comer_1.mp4";
+import Lancomer from "../assets/videos/3d/¡Ya llegó nuestra Temporada Naranja!.mp4";
 import Audi from "../assets/videos/3d/Audi R8. Un legado inolvidable.mp4";
 import Samsung from "../assets/videos/3d/SAMSUNG_BESPOKEIA.mp4";
 import Penafield from "../assets/videos/3d/PEÑAFIEL_LIMONYSAL_MASTER.mp4";
